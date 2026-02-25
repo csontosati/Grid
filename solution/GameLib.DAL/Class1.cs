eb724c28f6dde2a7c0661103bced1c870f94b78a
pivanek <ivanek@collabim.com>
2026-02-25 15:11:33 +0100

    Basic entity definition

--- solution/GameLib.DAL/Class1.cs
+++ solution/GameLib.DAL/Class1.cs
@@ -1,7 +1,61 @@
 ﻿namespace GameLib.DAL
 {
-    public class Class1
+    public record Game
     {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public string?  Description { get; set; }
+        public required string Category { get; set; }
+        public required string ImageUrl { get; set; }
+    }
 
+    public record Library
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+    }
+
+    public record User
+    {
+        public Guid Id { get; set; }
+        public required string UserName { get; set; }
+        public required string Email { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+
+    public record Studio 
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+    }
+
+    public enum GameCategory
+    {
+        Action,
+        Adventure,
+        RolePlaying,
+        Strategy,
+        Simulation,
+        Sports,
+        Racing,
+        Puzzle,
+        Shooter,
+        Fighting,
+        Platformer,
+        Survival,
+        Horror,
+        Sandbox,
+        MMO,
+        BattleRoyale,
+        Rhythm,
+        Card,
+        Board,
+        Educational,
+        Party,
+        Indie,
+        Arcade
     }
 }
