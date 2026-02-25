2569fa6b4690128ee9dd8a8def0b18547b82fb48
David Jadrný <dav.jadrny@gmail.com>
2026-02-25 17:16:47 +0100

    DbContext issue fix

--- solution/GameLib.DAL/GameLibDbContext.cs
+++ solution/GameLib.DAL/GameLibDbContext.cs
@@ -1,10 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using GameLib.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
 
-namespace GameLib.DAL
+namespace GameLib.DAL;
+
+public class GameLibDbContext : DbContext
 {
-    public class GameLibDbContext : DbContext
-    {
-    }
+    public DbSet<GameEntity> Games => Set<GameEntity>();
+    public DbSet<StudioEntity> Studios => Set<StudioEntity>();
+    public DbSet<LibraryEntity> Libraries => Set<LibraryEntity>();
+    public DbSet<UserEntity> Users=> Set<UserEntity>();
+
 }
