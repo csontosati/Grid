e171d57c428059927c8c14819fb173eb35a41c3a
csontosati <csontos.ati14@gmail.com>
2026-03-04 12:58:43 +0100

    New entities: TimerEntity, GameCategory Entity New enum: Pegi

---
+++ solution/GameLib.DAL/Entities/GameCategoryEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GameLib.DAL.Enums;
+
+namespace GameLib.DAL.Entities
+{
+    internal record GameCategoryEntity : IEntity
+    {
+        public Guid Id { get; set; }
+        public GameCategory TypeCategory { get; set; }
+
+    }
+}
---
+++ solution/GameLib.DAL/Entities/TimerEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameLib.DAL.Entities
+{
+    internal record TimerEntity : IEntity
+    {
+        public Guid Id { get; set; }
+        public TimeSpan Time { get; set; }
+    }
+}
