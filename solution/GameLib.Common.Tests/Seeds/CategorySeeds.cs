fe479df76c067710a6cb27ab8cb492e3f7539288
David Jadrný <dav.jadrny@gmail.com>
2026-03-08 18:37:45 +0100

    Moved seeds to the correct directory

---
+++ solution/GameLib.Common.Tests/Seeds/CategorySeeds.cs
@@ -0,0 +1,26 @@
+﻿using GameLib.DAL.Entities;
+using GameLib.DAL.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameLib.Common.Tests.Seeds;
+
+public static class CategorySeeds
+{
+    public static CategoryEntity ActionCategory => new()
+    {
+        Id = Guid.Parse("8F4B97B3-4785-42DB-8ED2-46174A222160"),
+        Category = GameCategory.Action
+    };
+
+    public static CategoryEntity MMOCategory => new()
+    {
+        Id = Guid.Parse("BC6D0F99-47FE-4060-ABAF-2C1B698B05B8"),
+        Category = GameCategory.MMO
+    };
+
+    public static DbContext SeedCategories(this DbContext dbx)
+    {
+        dbx.Set<CategoryEntity>().AddRange(ActionCategory, MMOCategory);
+        return dbx;
+    }
+}
