bf7da317342f4d5d08861bf5a65540a6ba7021b9
David Jadrný <dav.jadrny@gmail.com>
2026-05-19 10:22:02 +0200

    Updated UserFacade, added filtering

--- solution/GameLib.BL/Facades/UserFacade.cs
+++ solution/GameLib.BL/Facades/UserFacade.cs
@@ -1,11 +1,11 @@
-﻿using GameLib.BL.Mappers;
+﻿using System.Reflection.Metadata.Ecma335;
+using GameLib.BL.Mappers;
 using GameLib.BL.Models;
 using GameLib.BL.Facades.Interfaces;
 using GameLib.DAL.Entities;
-using GameLib.DAL.Enums;
 using GameLib.DAL.Mappers;
 using GameLib.DAL.UnitOfWork;
-using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace GameLib.BL.Facades;
 
@@ -17,8 +17,63 @@ public class UserFacade(
         UserListModel,
         UserDetailModel,
         UserEntityMapper>(
-            unitOfWorkFactory,
-            modelMapper)
+        unitOfWorkFactory,
+        modelMapper)
 {
-  
+    protected override ICollection<string> IncludesNavigationPathDetail =>
+        new[]
+        {
+            nameof(UserEntity.Libraries)
+        };
+
+    public class Filter
+    {
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
+        public string? OrderBy { get; set; }
+    }
+
+    protected override IQueryable<UserEntity> ApplyFilter(
+        IQueryable<UserEntity> query,
+        object? filter)
+    {
+        if (filter is not Filter f)
+            return query;
+
+        if (!string.IsNullOrWhiteSpace(f.UserName))
+            query = query.Where(u => u.UserName.Contains(f.UserName));
+
+        if (!string.IsNullOrWhiteSpace(f.Email))
+            query = query.Where(u => u.Email.Contains(f.Email));
+
+        return query;
+    }
+
+    protected override IQueryable<UserEntity> ApplyOrder(
+        IQueryable<UserEntity> query,
+        object? filter)
+    {
+        if (filter is not Filter f || string.IsNullOrWhiteSpace(f.OrderBy))
+            return query;
+
+        return f.OrderBy switch
+        {
+            "username" => query.OrderBy(u => u.UserName),
+            "username_desc" => query.OrderByDescending(u => u.UserName),
+            "email" => query.OrderBy(u => u.Email),
+            _ => query
+        };
+    }
+
+    public async Task<UserDetailModel?> GetUsernameAsync(string username)
+    {
+        await using var uow = UnitOfWorkFactory.Create();
+
+        var entity = await uow.GetRepository<UserEntity, UserEntityMapper>()
+            .Get()
+            .Include(u => u.Libraries)
+            .SingleOrDefaultAsync(u => u.UserName == username);
+        
+        return entity is null ? null : ModelMapper.MapToDetailModel(entity);
+    }
 }
