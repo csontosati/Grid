bca48ff4215ce7b151e16bce563ec210cc9a282d
csontosati <csontos.ati14@gmail.com>
2026-05-20 17:21:31 +0200

    UserSettingViewModel a View

--- solution/GameLib.App/ViewModels/UserSettingViewModel.cs
+++ solution/GameLib.App/ViewModels/UserSettingViewModel.cs
@@ -3,6 +3,7 @@ using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
 using GameLib.App.Messages;
 using GameLib.App.Services;
+using GameLib.BL.Facades;
 using GameLib.BL.Facades.Interfaces;
 using GameLib.BL.Models;
 using GameLib.DAL.Entities;
@@ -11,6 +12,7 @@ namespace GameLib.App.ViewModels;
 
 public partial class UserSettingsViewModel(
     IFacade<UserEntity, UserListModel, UserDetailModel> userFacade,
+    LibraryFacade libraryFacade,
     IMessengerService messengerService)
     : ViewModelBase(messengerService), IRecipient<UserSelectedMessage>
 {
@@ -19,12 +21,17 @@ public partial class UserSettingsViewModel(
     [ObservableProperty]
     public partial UserDetailModel? User { get; set; }
 
+    [ObservableProperty]
+    public partial string NewLibraryName { get; set; } = string.Empty;
+
     public void Receive(UserSelectedMessage message)
     {
         _currentUserId = message.UserId;
         ForceDataRefreshOnNextAppearing();
+        MainThread.BeginInvokeOnMainThread(async () => await LoadAsync());
     }
 
+    [RelayCommand]
     protected override async Task LoadAsync()
     {
         if (_currentUserId == Guid.Empty) return;
@@ -50,5 +57,28 @@ public partial class UserSettingsViewModel(
         if (_currentUserId == Guid.Empty) return;
         await userFacade.DeleteAsync(_currentUserId);
         await Shell.Current.GoToAsync(NavigationService.LandingPageRouteAbsolute);
+    }
+
+    [RelayCommand]
+    private async Task AddLibraryAsync()
+    {
+        if (string.IsNullOrWhiteSpace(NewLibraryName) || User is null) return;
+        var library = new LibraryDetailModel
+        {
+            Id = Guid.Empty,
+            Name = NewLibraryName,
+            UserId = _currentUserId
+        };
+        await libraryFacade.SaveAsync(library);
+        NewLibraryName = string.Empty;
+        ForceDataRefreshOnNextAppearing();
+        await LoadAsync();
+    }
+
+    [RelayCommand]
+    private async Task DeleteLibraryAsync(LibraryListModel library)
+    {
+        await libraryFacade.DeleteAsync(library.Id);
+        User?.Libraries.Remove(library);
     }
 }
