f8aa9f4459ff5e1072ee7f66d240403b78c5838f
pivanek <ivanek@collabim.com>
2026-05-20 14:47:31 +0200

    GameListViewModel

---
+++ solution/GameLib.App/ViewModels/GameListViewModel.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using GameLib.App.Services;
+using GameLib.App.Services.Interfaces;
+using GameLib.BL.Facades.Interfaces;
+using GameLib.BL.Models;
+using GameLib.DAL.Entities;
+
+namespace GameLib.App.ViewModels;
+public partial class GameListViewModel(
+    IGameFacade gameFacade,
+    INavigationService navigationService,
+    IMessengerService messengerService)
+: ViewModelBase(messengerService)
+{
+    [ObservableProperty]
+    public partial IEnumerable<GameListModel> Games { get; set; } = [];
+
+    [RelayCommand]
+    protected async Task LoadGamesAsync()
+    {
+        await base.LoadAsync();
+        
+        Games = await gameFacade.GetAsync();
+    }
+}
---
+++ solution/GameLib.BL/Facades/Interfaces/IGameFacade.cs
@@ -0,0 +1,11 @@
+﻿using GameLib.BL.Models;
+using GameLib.DAL.Entities;
+
+namespace GameLib.BL.Facades.Interfaces;
+
+public interface IGameFacade : IFacade<GameEntity, GameListModel, GameDetailModel>
+{
+    protected ICollection<string> IncludesNavigationPathDetails();
+    protected IQueryable<GameEntity> ApplyFilter(IQueryable<GameEntity> query, object? filter);
+    protected IQueryable<GameEntity> ApplyOrder(IQueryable<GameEntity> query, object? filter);
+}
