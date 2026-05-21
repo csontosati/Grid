2978d40cdb330804100bb89a8dbdaf002f750d46
oc-22 <cagalinec.o@gmail.com>
2026-05-21 02:51:57 +0200

    Initial filtering application

--- solution/GameLib.App/ViewModels/GameListViewModel.cs
+++ solution/GameLib.App/ViewModels/GameListViewModel.cs
@@ -4,20 +4,40 @@ using CommunityToolkit.Mvvm.Messaging;
 using GameLib.App.Messages;
 using GameLib.App.Services;
 using GameLib.App.Services.Interfaces;
-using GameLib.App.Views;
+using GameLib.BL.Facades;
 using GameLib.BL.Facades.Interfaces;
 using GameLib.BL.Models;
 using GameLib.DAL.Entities;
+using GameLib.DAL.Enums;
 
 namespace GameLib.App.ViewModels;
+
 public partial class GameListViewModel(
     IFacade<GameEntity, GameListModel, GameDetailModel> gameFacade,
     INavigationService navigationService,
     IMessengerService messengerService)
-: ViewModelBase(messengerService), IRecipient<GameAddedMessage>, IRecipient<UserSelectedMessage>, IRecipient<GameDeletedMessage>, IRecipient<GameUpdatedMessage>
+    : ViewModelBase(messengerService),
+      IRecipient<GameAddedMessage>,
+      IRecipient<UserSelectedMessage>,
+      IRecipient<GameDeletedMessage>,
+      IRecipient<GameUpdatedMessage>
 {
     [ObservableProperty]
     public partial IEnumerable<GameListModel> Games { get; set; } = [];
+
+    private GameFacade.Filter? _currentFilter;
+
+    [ObservableProperty]
+    public partial string? FilterName { get; set; }
+
+    [ObservableProperty]
+    public partial int? FilterAge { get; set; }
+
+    [ObservableProperty]
+    public partial string? SelectedOrderBy { get; set; }
+
+    public Array PegiValues => Enum.GetValues(typeof(Pegi));
+
 
     private Guid _currentUserId = Guid.Empty;
 
@@ -25,45 +45,70 @@ public partial class GameListViewModel(
     protected override async Task LoadAsync()
     {
         await base.LoadAsync();
+
+        Games = await gameFacade.GetAsync(_currentFilter);
+    }
+
+    [RelayCommand]
+    private async Task ApplyFilterAsync()
+    {
+        _currentFilter = new GameFacade.Filter
+        {
+            Name = FilterName,
+            Age = (Pegi?)FilterAge,
+            OrderBy = SelectedOrderBy
+        };
+
+        Games = await gameFacade.GetAsync(_currentFilter);
+    }
+
+    [RelayCommand]
+    private async Task ClearFilterAsync()
+    {
+        FilterName = null;
+        FilterAge = null;
+        SelectedOrderBy = null;
+
+        _currentFilter = null;
+
         Games = await gameFacade.GetAsync();
     }
 
     [RelayCommand]
     private async Task GoToAddGameAsync()
     {
-        await navigationService.GoToAsync(NavigationService.GameAddPageRouteAbsolute);
-    }
-    public void Receive(GameAddedMessage message)
-    {
-        ForceDataRefreshOnNextAppearing();
-    }
-    public void Receive(UserSelectedMessage message)
-    {
-        _currentUserId = message.UserId;
-        System.Diagnostics.Debug.WriteLine($"UserSelectedMessage received: UserId = {_currentUserId}");
-    }
-    public void Receive(GameDeletedMessage message)
-    {
-        ForceDataRefreshOnNextAppearing();
-    }
-    public void Receive(GameUpdatedMessage message)
-    {
-        ForceDataRefreshOnNextAppearing();
+        await navigationService.GoToAsync(
+            NavigationService.GameAddPageRouteAbsolute);
     }
 
+    public void Receive(GameAddedMessage message)
+        => ForceDataRefreshOnNextAppearing();
+
+    public void Receive(UserSelectedMessage message)
+        => _currentUserId = message.UserId;
+
+    public void Receive(GameDeletedMessage message)
+        => ForceDataRefreshOnNextAppearing();
+
+    public void Receive(GameUpdatedMessage message)
+        => ForceDataRefreshOnNextAppearing();
 
     [RelayCommand]
     private async Task GoToUserSettingsAsync()
     {
-        if (_currentUserId == Guid.Empty) return;
+        if (_currentUserId == Guid.Empty)
+            return;
 
-        await navigationService.GoToAsync(NavigationService.UserSettingsPageAbsolute);
+        await navigationService.GoToAsync(
+            NavigationService.UserSettingsPageAbsolute);
     }
+
     [RelayCommand]
     private async Task GoToGameDetailAsync(GameListModel game)
     {
         messengerService.Send(new GameSelectedMessage(game.Id));
-        await navigationService.GoToAsync(NavigationService.GameDetailPageAbsolute);
-    }
 
+        await navigationService.GoToAsync(
+            NavigationService.GameDetailPageAbsolute);
+    }
 }
